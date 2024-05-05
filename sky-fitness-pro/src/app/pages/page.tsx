e86d7212e6f25ebb746d05102b58ae9badf6f9b5
Svetlana Kosuhina <142496854+Lanakosuhina@users.noreply.github.com>
2024-05-05 22:52:44 +0300

    Изменен layout, убрала отлельный компонент

--- sky-fitness-pro/src/app/pages/page.tsx
+++ sky-fitness-pro/src/app/pages/page.tsx
@@ -1,7 +1,27 @@
-import CoursesLayout from "@/components/CoursesLayout/CoursesLayout";
+import Button from "@/components/Button/Button";
+import CourseCard from "@/components/CourseCard/CourseCard";
 
 export default function MainCoursesPage() {
   return (
-    <CoursesLayout />
+    <>
+      <div className="flex flex-row mb-[58px] items-end space-x-7">
+        <h1 className="font-roboto-500 text-[32px] md:text-[40px]/[60px] xl:text-[60px]/[60px] text-pretty">
+          Начните заниматься спортом<br /> и улучшите качество жизни
+        </h1>
+        <svg className="hidden sm:block sm:h-[120px]">
+          <use xlinkHref={`/img/sprite.svg#icon-notice`}></use>
+        </svg>
+      </div>
+      <div className="flex flex-wrap gap-x-10 gap-y-8">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+      <div className="flex self-center w-[140px]">
+        <Button title="Наверх &#8593;" />
+      </div>
+    </>
   )
 }
