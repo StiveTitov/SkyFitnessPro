7bf14ebee3bd5e188eed8d75080b5173c888ee7f
StiveTitov <123_dem_321@mail.ru>
2024-05-06 22:50:09 +0300

    начал делать модальное окно SelectWorkout

---
+++ sky-fitness-pro/src/components/SelectWorkout/SelectWorkout.tsx
@@ -0,0 +1,12 @@
+export default function SelectWorkout() {
+    return (
+        <>
+        <div className="bg-[#FFFFFF]
+                    rounded-[30px] 
+                    sm:w-[460px] w-[343px]
+                    sm:h-[609px] h-[585px]">
+
+        </div>
+        </>
+    )
+}
