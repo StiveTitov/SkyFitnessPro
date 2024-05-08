ed63f862f40e13ffbb301c2e669a86ddb03a172f
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2024-05-08 15:02:34 +0300

    Добавила якорную ссылку на кнопку "Наверх", изменила падинг для кнопок по х-оси

--- sky-fitness-pro/src/components/ButtonLink/ButtonLink.tsx
+++ sky-fitness-pro/src/components/ButtonLink/ButtonLink.tsx
@@ -7,7 +7,7 @@ type ButtonLinkType = {
 
 export default function ButtonLink({ title, link }: ButtonLinkType) {
   return (
-    <button className="rounded-full border border-black w-full h-[52px] px-7 bg-transparent text-lg text-[#000000] hover:bg-[#F7F7F7] active:bg-[#E9ECED]">
+    <button className="rounded-full border border-black w-full h-[52px] px-5 bg-transparent text-lg text-[#000000] hover:bg-[#F7F7F7] active:bg-[#E9ECED]">
       <Link href={link}>{title}</Link>
     </button>
   );
