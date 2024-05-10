3c2ece70bb1f00f51f6a59e742b04a8858e8b0ea
ZakhTania <tatizarova@gmail.com>
2024-05-10 13:40:05 +0300

    Изменены стили

--- sky-fitness-pro/src/app/pages/workout/page.tsx
+++ sky-fitness-pro/src/app/pages/workout/page.tsx
@@ -16,25 +16,25 @@ export default function WorkoutPage() {
           items={breadcrumbsItems}
         />
       </section>
-      <section className="h-[189px] lg:h-[639px] rounded-[30px] mb-6 lg:mb-10">
+      <section className="h-[189px] md:min-h-fit lg:h-[639px] rounded-[30px] mb-6 lg:mb-10">
         <Suspense fallback={<p>Loading video...</p>}>
           <VideoComponent />
         </Suspense>
       </section>
       <section className="rounded-[30px] p-[30px] lg:p-10 bg-white shadow-def ">
-        <h2 className="text-[32px] text-black font-skyeng font-normal">
+        <h2 className="text-[32px] text-black font-skyeng font-normal mb-[20px]">
           Упражнения тренировки 2
         </h2>
-        <div className="min-h-fit flex flex-col gap-6 lg:grid lg:gap-x-14 lg:gap-y-5 grid-rows-3 grid-cols-3 grid-flow-col items-end">
+        <div className="grid grid-flow-row gap-6 items-end md:grid-cols-2 md:gap-5 lg:grid-cols-3">
           {workoutProgress.map((step, i) => {
             return (
-              <div className="lg:w-[320px] min-w-[283px] w-auto" key={i}>
+              <div className="lg:w-[320px] w-[283px]" key={i}>
                 <WorkoutProgress title={step.title} progress={step.process} />
               </div>
             );
           })}
         </div>
-        <div className="lg:w-[320px] min-w-[283px] w-auto mt-10">
+        <div className="lg:w-[320px] max-w-[283px] w-auto mt-10">
           <Button title="Заполнить свой прогресс" />
         </div>
       </section>
